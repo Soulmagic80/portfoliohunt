192e385f1e4c2b2a1b502b3b2306337ca09cb3b2
Philippe La Roche <philippe@Mac.localdomain>
2025-03-04 22:34:23 +0100

    Updated Navbar alignment, added fonts to layout and tailwind config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import { Inter } from "next/font/google";
 import { Courier_Prime } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
-const courierPrime = Courier_Prime({ subsets: ["latin"], weight: ["400"] }); // Regular für Navbar
 
 
 export const metadata: Metadata = {
